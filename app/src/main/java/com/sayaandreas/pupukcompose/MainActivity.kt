510294910ce713be4ddab6cb22907ee5d9980f1d
sayaandreas <sayaandreas@gmail.com>
2020-12-24 17:04:40 +0700

    add multinavhost navigation

--- app/src/main/java/com/sayaandreas/pupukcompose/MainActivity.kt
+++ app/src/main/java/com/sayaandreas/pupukcompose/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.savedinstancestate.Saver
+import androidx.compose.runtime.savedinstancestate.rememberSavedInstanceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
@@ -22,6 +22,9 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.os.bundleOf
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 import androidx.ui.tooling.preview.Preview
@@ -34,8 +37,97 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Routes(mainViewModel)
+            MultiBottomNavApp(mainViewModel)
         }
+    }
+}
+
+@Composable
+fun MultiBottomNavApp(mainViewModel: MainViewModel) {
+    BottomNavApp {
+        MultiNavTabContent(screen = it, mainViewModel)
+    }
+}
+
+@Composable
+fun MultiNavTabContent(screen: Screen, mainViewModel: MainViewModel) {
+    val gardenNavState =
+        rememberSavedInstanceState(saver = NavStateSaver()) { mutableStateOf(Bundle()) }
+    when (screen) {
+        Screen.Garden -> GardenTab(gardenNavState, mainViewModel)
+        Screen.Explore -> Explore()
+        Screen.Reminders -> Reminders()
+        else -> Profile()
+    }
+}
+
+@Composable
+fun GardenTab(navState: MutableState<Bundle>, mainViewModel: MainViewModel) {
+    val navController = rememberNavController()
+
+    onCommit {
+        val callback = NavController.OnDestinationChangedListener { controller, _, _ ->
+            navState.value = controller.saveState() ?: Bundle()
+        }
+        navController.addOnDestinationChangedListener(callback)
+        navController.restoreState(navState.value)
+
+        onDispose {
+            navController.removeOnDestinationChangedListener(callback)
+            navController.enableOnBackPressed(false)
+        }
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Garden.route
+    ) {
+        composable(Screen.Garden.route) { Garden(navController, mainViewModel) }
+        composable(Screen.Plant.route) { Plant(it.arguments?.get("plantId") as String) }
+    }
+}
+
+@Composable
+fun Garden(navController: NavController, mainViewModel: MainViewModel) {
+    val items: List<Product> by mainViewModel.productList.observeAsState(listOf())
+    LazyColumn {
+        items(items) { product ->
+            Product(
+                product,
+                onClick = {
+                    navController.navigate(
+                        Screen.Plant.routeWithPhrase(product.title)
+                    )
+                })
+        }
+    }
+}
+
+@Composable
+fun Plant(plantId: String) {
+    Column(modifier = Modifier.fillMaxSize().then(Modifier.padding(8.dp))) {
+        Text(text = plantId)
+    }
+}
+
+@Composable
+fun Explore() {
+    Column(modifier = Modifier.fillMaxSize().then(Modifier.padding(8.dp))) {
+        Text(text = Screen.Explore.route)
+    }
+}
+
+@Composable
+fun Reminders() {
+    Column(modifier = Modifier.fillMaxSize().then(Modifier.padding(8.dp))) {
+        Text(text = Screen.Reminders.route)
+    }
+}
+
+@Composable
+fun Profile() {
+    Column(modifier = Modifier.fillMaxSize().then(Modifier.padding(8.dp))) {
+        Text(text = Screen.Profile.route)
     }
 }
 
@@ -54,9 +146,9 @@ fun AsyncImage(url: String) {
 @Composable
 fun Product(
     product: Product,
-    selectedProduct: (product: Product) -> Unit
+    onClick: () -> Unit
 ) {
-    Row(modifier = Modifier.clickable(onClick = { selectedProduct(product) })) {
+    Row(modifier = Modifier.clickable(onClick = { onClick() })) {
         Card(
             Modifier.weight(1f).padding(start = 16.dp, end = 8.dp, top = 8.dp, bottom = 8.dp),
             elevation = 4.dp
@@ -86,57 +178,103 @@ fun Product(
 }
 
 @Composable
-fun HomeScreen(mainViewModel: MainViewModel, selectedProduct: (product: Product) -> Unit) {
-    val items: List<Product> by mainViewModel.productList.observeAsState(listOf())
-    LazyColumn {
-        items(items) {
-            Product(it, selectedProduct)
-        }
+fun BottomNavApp(
+    bodyContent: @Composable (Screen) -> Unit
+) {
+    var currentTab by rememberSavedInstanceState(saver = ScreenSaver()) {
+        mutableStateOf(
+            Screen.Garden
+        )
     }
-}
 
-@Composable
-fun DetailScreen(mainViewModel: MainViewModel, id: Int) {
-    val product = mainViewModel.getProductDetail(id)
-    product?.let { Text(text = it.title) }
-}
-
-sealed class Screen(val title: String) {
-    object HomeScreen : Screen("HomeScreen")
-    object DetailScreen : Screen("DetailScreen")
-}
-
-@Composable
-fun Routes(mainViewModel: MainViewModel) {
-    val navController = rememberNavController()
     PupukComposeTheme {
         Surface(color = MaterialTheme.colors.background) {
-            NavHost(
-                navController = navController,
-                startDestination = Screen.HomeScreen.title
-            ) {
-                composable(Screen.HomeScreen.title) {
-                    HomeScreen(mainViewModel,
-                        selectedProduct = {
-                            navController.navigate(Screen.DetailScreen.title + "/${it.id}")
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(currentTab.route)
                         }
                     )
-                }
-                composable(
-                    Screen.DetailScreen.title + "/{id}",
-                    arguments = listOf(navArgument("id") { type = NavType.IntType })
-                ) { backStackEntry ->
-                    backStackEntry.arguments?.getInt("id")?.let {
-                        DetailScreen(
-                            mainViewModel,
-                            it
+                }, bodyContent = {
+                    bodyContent(currentTab)
+                },
+                bottomBar = {
+                    BottomNavigation {
+                        BottomNavigationItem(
+                            icon = { Icon(Icons.Default.AccountCircle) },
+                            label = { Text("Garden") },
+                            selected = currentTab == Screen.Garden,
+                            onClick = { currentTab = Screen.Garden }
+                        )
+                        BottomNavigationItem(
+                            icon = { Icon(Icons.Default.ShoppingCart) },
+                            label = { Text("Explore") },
+                            selected = currentTab == Screen.Explore,
+                            onClick = { currentTab = Screen.Explore }
+                        )
+                        BottomNavigationItem(
+                            icon = { Icon(Icons.Default.List) },
+                            label = { Text("Reminders") },
+                            selected = currentTab == Screen.Reminders,
+                            onClick = { currentTab = Screen.Reminders }
+                        )
+                        BottomNavigationItem(
+                            icon = { Icon(Icons.Default.List) },
+                            label = { Text("Profile") },
+                            selected = currentTab == Screen.Profile,
+                            onClick = { currentTab = Screen.Profile }
                         )
                     }
                 }
-            }
+            )
         }
     }
 }
+
+fun NavBackStackEntry.getRoute(): String {
+    return arguments?.getString(KEY_ROUTE) ?: ""
+}
+
+sealed class Screen(val route: String) {
+    object Garden : Screen("Garden")
+    object Explore : Screen("Explore")
+    object Reminders : Screen("Reminders")
+    object Profile : Screen("Profile")
+    object Plant : Screen("Plant/?plantId={plantId}") {
+        fun routeWithPhrase(plantId: String): String = route.replace("{plantId}", plantId)
+    }
+
+    fun saveState(): Bundle {
+        return bundleOf(KEY_SCREEN to route)
+    }
+
+    companion object {
+        fun restoreState(bundle: Bundle): Screen {
+            return when (bundle.getString(KEY_SCREEN, Profile.route)) {
+                Garden.route -> Garden
+                Plant.route -> Plant
+                Explore.route -> Explore
+                Reminders.route -> Reminders
+                Profile.route -> Profile
+                else -> Garden
+            }
+        }
+
+        const val KEY_SCREEN = "route"
+    }
+}
+
+fun ScreenSaver(
+): Saver<MutableState<Screen>, *> = Saver(
+    save = { it.value.saveState() },
+    restore = { mutableStateOf(Screen.restoreState(it)) }
+)
+
+fun NavStateSaver(): Saver<MutableState<Bundle>, out Any> = Saver(
+    save = { it.value },
+    restore = { mutableStateOf(it) }
+)
 
 @Preview(showBackground = true)
 @Composable
